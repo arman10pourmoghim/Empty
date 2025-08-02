b7d32ceb83d5ee96af82756c462f4d3014011d84
arman10pourmoghim <armanpr0710goat@gmail.com>
2025-08-02 16:51:22 +0330

    Final modification

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Python beginner tutorial:
 1- print vs. f string
 
 2- Comparing while loop and for loop
+
+3- conditional statements
+
+4- Every body can contribute to this repository and complete it.
--- index.py
+++ index.py
@@ -19,3 +19,16 @@ while i < 10:
 print("-"*10)
 for j in range(10):
     print(j)
+
+print("_"*10)
+
+number= 62
+while True:
+    guess= int(input("Guess the number."))
+    if guess < number:
+        print("It is higher.")
+    elif guess > number:
+        print("It is lower.")
+    else:
+        print("That is right.")
+        break
